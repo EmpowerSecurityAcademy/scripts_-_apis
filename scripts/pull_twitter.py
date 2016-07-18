93b97517656bda63c2bda45b74494111810cc423
sheltowt <sheltowt@gmail.com>
2016-07-18 10:39:03 -0400

    adding updates

--- scripts/pull_twitter.py
+++ scripts/pull_twitter.py
@@ -1,2 +1,25 @@
 import tweepy
 import pymongo
+from import_config import load_config
+from pymongo import MongoClient
+
+config = load_config()
+
+client = MongoClient(config["database"]["connection_url"])
+
+db = client[config['database']['database_name']]
+basic_api.conn = db[config['database']['collection_name']]
+
+def get_tweets():
+
+	auth = tweepy.OAuthHandler(config["twitter"]["consumer_key"], config["database"]["consumer_secret"])
+	auth.set_access_token(config["twitter"]["access_key"], config["twitter"]["access_secret"])
+	api = tweepy.API(auth)
+
+	new_tweets = api.user_timeline(screen_name = "empowersec", count=200)
+
+	for tweet in new_tweets:
+		print(tweet)
+
+if __name__ == '__main__':
+	get_tweets()
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(name='automated_workflows',
+      version='1.0',
+      description='Tutorial around automated workflows',
+      author='Bill Shelton',
+      author_email='bill@empowersecurityacademy.com',
+      url='http://wwww.empowersecurityacademy.com',
+      install_requires=["tweepy"],
+     )
