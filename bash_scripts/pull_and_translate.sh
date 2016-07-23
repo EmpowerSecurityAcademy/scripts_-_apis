399eadc3b5aff6f71c27aef4683ed500e759c014
sheltowt <sheltowt@gmail.com>
2016-07-23 10:44:03 -0400

    adding updates

---
+++ bash_scripts/pull_and_translate.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+python /Users/williamshelton/Code/scripts_-_apis/python_scripts/pull_twitter.py
+python /Users/williamshelton/Code/scripts_-_apis/python_scripts/parts_of_speech.py
+python /Users/williamshelton/Code/scripts_-_apis/python_scripts/google_translate.py
+python /Users/williamshelton/Code/scripts_-_apis/python_scripts/save_to_csv.py
